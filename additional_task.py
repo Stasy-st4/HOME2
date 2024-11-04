b1e8c86877807bfd9f1e2e49ebb7762ce635fdad
Stasy-st4 <ishtubaevaa@gmail.com>
2024-11-04 17:58:45 +0000

    Add files via upload

---
+++ additional_task.py
@@ -0,0 +1,4 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+average_grades = {student: sum(marks) / len(marks) for student, marks in zip(sorted(students), grades)}
+print(average_grades)
